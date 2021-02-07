e7094134d92f0c7074265e2390924613444c2647
jonatanivangonzalez1317 <jigonzalez1317@gmail.com>
2021-02-07 02:07:24 -0300

    primer commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("curso de ingreso");
+	alert("Bienvenido curso de ingreso");
 }
 
